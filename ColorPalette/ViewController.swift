33552ddacfabfcf6c89e01931a625be71c500f16
DaniilPetrov <dany_petrov@icloud.com>
2022-06-24 11:16:47 +0300

    Initial Commit

---
+++ ColorPalette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPalette
+//
+//  Created by Даниил Петров on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
